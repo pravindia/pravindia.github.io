848a5ada5ab0c38f3f59b798993a303d58ceb786
Pravin kumar <pravinda4hero@gmail.com>
2017-06-15 19:44:46 +0530

    two

---
+++ sw.js
@@ -0,0 +1,20 @@
+var cacheName = "offline";
+
+self.addEventListener("install", function (event) {
+    event.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            return cache.addAll([
+            "/index.html"
+        ]);
+        })
+    )
+});
+
+
+self.addEventListener("fetch", function (event) {
+    event.respondWith(
+        caches.open(cacheName).then(function (cache) {
+            return cache.match(event.request);
+        })
+    );
+});
